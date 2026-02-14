d3162688656ad30bf648754aa4c93410c7b6c967
CrlJason <yeisondamosquera@gmail.com>
2026-02-14 13:00:56 -0500

    feat: Se agrego el campo de films al modelo character - Se agrego el campo films al modelo character y al DTO create character

--- api/src/modules/characters/dto/characters.dto.ts
+++ api/src/modules/characters/dto/characters.dto.ts
@@ -9,7 +9,7 @@ export const createCharacterDto = [
     body("date")
         .notEmpty()
         .withMessage("La fecha de creación es obligatorio")
-        .isDate()
+        .isISO8601()
         .withMessage("La fecha debe ser estar en un formato valido"),
     body("description")
         .notEmpty()
@@ -21,6 +21,12 @@ export const createCharacterDto = [
         .withMessage("El Url es obligatorio")
         .isString()
         .withMessage("La URL debe ser un string"),
+    body("films")
+        .notEmpty()
+        .withMessage("El personaje debe tener almenos una película")
+        .isArray({ min: 1 })
+        .withMessage("Debe ser un array con al menos un elemento"),
+    body("films.*").isString().withMessage("Cada película debe ser un string"),
 ];
 
 export const getCharacterByIdDto = [
